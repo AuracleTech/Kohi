e5123a34263d5bd040df330949be00496496799f
AuracleTech <VirgilFabre@outlook.com>
2022-07-28 12:59:01 -0400

    Memory Subsystem

--- engine/src/core/application.c
+++ engine/src/core/application.c
@@ -4,6 +4,7 @@
 #include "logger.h"
 
 #include "platform/platform.h"
+#include "core/kmemory.h"
 
 typedef struct application_state
 {
@@ -70,6 +71,9 @@ b8 application_create(game *game_inst)
 
 b8 application_run()
 {
+    // TODO: Fix memory Leak
+    KINFO(get_memory_usage_str());
+
     while (app_state.is_running)
     {
         if (!platform_pump_messages(&app_state.platform))
