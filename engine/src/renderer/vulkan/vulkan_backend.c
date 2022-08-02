5bb64a61f1c028f37cb142cb64721d52a08cbdf8
AuracleTech <VirgilFabre@outlook.com>
2022-08-02 01:04:39 -0400

    Vulkan Command Pools and Buffers

--- engine/src/renderer/vulkan/vulkan_backend.c
+++ engine/src/renderer/vulkan/vulkan_backend.c
@@ -5,9 +5,11 @@
 #include "vulkan_device.h"
 #include "vulkan_swapchain.h"
 #include "vulkan_renderpass.h"
+#include "vulkan_command_buffer.h"
 
 #include "core/logger.h"
 #include "core/kstring.h"
+#include "core/kmemory.h"
 
 #include "containers/darray.h"
 
@@ -24,9 +26,10 @@ VKAPI_ATTR VkBool32 VKAPI_CALL vk_debug_callback(
 
 i32 find_memory_index(u32 type_filter, u32 property_flags);
 
+void create_command_buffers(renderer_backend *backend);
+
 b8 vulkan_renderer_backend_initialize(renderer_backend *backend, const char *application_name, struct platform_state *plat_state)
 {
-
     // Function pointers
     context.find_memory_index = find_memory_index;
 
@@ -155,7 +158,6 @@ b8 vulkan_renderer_backend_initialize(renderer_backend *backend, const char *app
         context.framebuffer_height,
         &context.swapchain);
 
-    // Renderpass
     vulkan_renderpass_create(
         &context,
         &context.main_renderpass,
@@ -164,6 +166,9 @@ b8 vulkan_renderer_backend_initialize(renderer_backend *backend, const char *app
         1.0f,
         0);
 
+    // Create command buffers.
+    create_command_buffers(backend);
+
     KINFO("Vulkan renderer initialized successfully.");
     return TRUE;
 }
@@ -171,6 +176,21 @@ b8 vulkan_renderer_backend_initialize(renderer_backend *backend, const char *app
 void vulkan_renderer_backend_shutdown(renderer_backend *backend)
 {
     // Destroy in the opposite order of creation.
+
+    // Command buffers
+    for (u32 i = 0; i < context.swapchain.image_count; ++i)
+    {
+        if (context.graphics_command_buffers[i].handle)
+        {
+            vulkan_command_buffer_free(
+                &context,
+                context.device.graphics_command_pool,
+                &context.graphics_command_buffers[i]);
+            context.graphics_command_buffers[i].handle = 0;
+        }
+    }
+    darray_destroy(context.graphics_command_buffers);
+    context.graphics_command_buffers = 0;
 
     // Renderpass
     vulkan_renderpass_destroy(&context, &context.main_renderpass);
@@ -188,6 +208,7 @@ void vulkan_renderer_backend_shutdown(renderer_backend *backend)
         context.surface = 0;
     }
 
+#if defined(_DEBUG)
     KDEBUG("Destroying Vulkan debugger...");
     if (context.debug_messenger)
     {
@@ -195,6 +216,7 @@ void vulkan_renderer_backend_shutdown(renderer_backend *backend)
             (PFN_vkDestroyDebugUtilsMessengerEXT)vkGetInstanceProcAddr(context.instance, "vkDestroyDebugUtilsMessengerEXT");
         func(context.instance, context.debug_messenger, context.allocator);
     }
+#endif
 
     KDEBUG("Destroying Vulkan instance...");
     vkDestroyInstance(context.instance, context.allocator);
@@ -255,4 +277,35 @@ i32 find_memory_index(u32 type_filter, u32 property_flags)
 
     KWARN("Unable to find suitable memory type!");
     return -1;
+}
+
+void create_command_buffers(renderer_backend *backend)
+{
+    if (!context.graphics_command_buffers)
+    {
+        context.graphics_command_buffers = darray_reserve(vulkan_command_buffer, context.swapchain.image_count);
+        for (u32 i = 0; i < context.swapchain.image_count; ++i)
+        {
+            kzero_memory(&context.graphics_command_buffers[i], sizeof(vulkan_command_buffer));
+        }
+    }
+
+    for (u32 i = 0; i < context.swapchain.image_count; ++i)
+    {
+        if (context.graphics_command_buffers[i].handle)
+        {
+            vulkan_command_buffer_free(
+                &context,
+                context.device.graphics_command_pool,
+                &context.graphics_command_buffers[i]);
+        }
+        kzero_memory(&context.graphics_command_buffers[i], sizeof(vulkan_command_buffer));
+        vulkan_command_buffer_allocate(
+            &context,
+            context.device.graphics_command_pool,
+            TRUE,
+            &context.graphics_command_buffers[i]);
+    }
+
+    KDEBUG("Vulkan command buffers created.");
 }
